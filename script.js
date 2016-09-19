264b5da970fe4a20ecab32c3c6ad8114e0074e57
Ihor Motornyi <igormotorny1997@gmail.com>
2016-09-19 22:14:11 +0300

    Added test script

---
+++ script.js
@@ -0,0 +1,21 @@
+document.onload = function() {
+  var script = document.createElement('script');
+  script.onLoad = function() {
+    hljs.initHighlightingOnLoad();
+  }
+  script.setAttribute('src', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.6.0/highlight.min.js';
+  document.body.appendChild(script);
+
+  var style = document.createElement(link);
+  style.setAttribute('rel', 'stylesheet');
+  style.setAttribute('type', 'text/css');
+  style.setAttribute('href', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.6.0/styles/default.min.css');
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elements = document.getElementsByTagName('code');
+  elements.forEach(function(el) {
+    el.classList.add('')
+  })
+ });
