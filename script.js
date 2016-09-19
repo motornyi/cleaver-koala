e28e718ecc48a7556f151eb8164648a509a64ce7
Ihor Motornyi <igormotorny1997@gmail.com>
2016-09-19 22:24:07 +0300

    Auto commit

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ document.onload = function() {
   style.setAttribute('type', 'text/css');
   style.setAttribute('href', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.6.0/styles/default.min.css');
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elements = document.getElementsByTagName('code');
+  console.log(elements);
+  elements.forEach(function(el) {
+    el.classList.add('')
+  })
+ });
